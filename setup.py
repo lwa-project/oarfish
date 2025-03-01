58e92354756a83b24a624b9f74bf1e55c0a4c776
jaycedowell <jaycedowell@gmail.com>
2025-02-28 18:31:19 -0700

    Initial binary and multi-class models.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+import glob
+import shutil
+
+from setuptools import setup, find_namespace_packages
+
+setup(name                 = "oarfish",
+      version              = "0.1.0",
+      description          = "LWATV image classifier",
+      long_description     = "Deep learning for classifying LWATV images",
+      author               = "J. Dowell",
+      author_email         = "jdowell@unm.edu",
+      license              = 'BSD3',
+      packages             = find_namespace_packages(),
+      scripts              = glob.glob('scripts/*.py'),
+      python_requires      = '>=3.8',
+      install_requires     = ['numpy',],
+      zip_safe             = False
+)
