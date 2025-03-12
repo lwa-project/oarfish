543231df70361a41665ecfceca9db5721991ccda
jaycedowell <jaycedowell@gmail.com>
2025-03-12 09:59:11 -0600

    Bring stats to the server as well.

--- oarfish/server.py
+++ oarfish/server.py
@@ -4,6 +4,8 @@ import queue
 import numpy as np
 from logging import Logger
 from functools import lru_cache
+from collections import deque
+
 from typing import Optional, Any, Tuple, Union
 
 from torch.utils.data import DataLoader
@@ -21,12 +23,36 @@ class PredictionServer:
         self.predictor = predictor
         self.logger = logger
         
+        self.request_stats = {'start_time': -1.0,
+                              'total': 0,
+                              'send_failed': 0,
+                              'error': 0,
+                              'success': 0,
+                              'timeout': 0,
+                              'last_success': -1.0,
+                              'response_times': deque([], maxlen=100)
+                             }
+        
+    def _reset_stats(self):
+        """
+        Reset the connection statistics to a clean state.
+        """
+        
+        for key in ('start_time', 'last_success'):
+            self.request_stats[key] = -1.0
+        for key in ('total', 'send_failed', 'error', 'success', 'timeout'):
+            self.request_stats[key] = 0
+        self.request_stats['response_times'].clear()
+        
     def start(self):
         self.ctx = zmq.Context()
         self.sock = self.ctx.socket(zmq.ROUTER)
         
         self.sock.setsockopt(zmq.LINGER, 0)
         self.sock.setsockopt(zmq.RCVTIMEO, int(self.timeout*1000))
+        
+        self._reset_stats()
+        self.request_stats['start_time'] = time.time()
         
         self.sock.bind(f"tcp://{self.address}:{self.port}")
         
@@ -40,9 +66,46 @@ class PredictionServer:
         if self.logger:
             self.logger.info(f"Stopped prediction server on {self.address} port {self.port}")
             
+    ef get_stats() -> Dict[str, Any]:
+        """
+        Get connection statistics about the client that include:
+         * how long it has been since start() was called
+         * how many requests have been processed
+         * when the last requests was successfully processed
+         * the average processing time of the last 100 requests
+        """
+        
+        uptime = 0.0
+        if self.request_stats['start_time'] > 0:
+            uptime = time.time() - self.request_stats['start_time']
+            
+        resp_time = 0.0
+        if self.request_stats['response_times']:
+            resp_time = sum(self.request_stats['response_times']) \
+                        / len(self.request_stats['response_times'])
+        
+        last_good = None
+        if self.request_stats['last_success'] > 0:
+            last_good = self.request_stats['last_success']
+            
+        return {'running': self.ctx is not None,
+                'uptime': uptime,
+                'requests': {'total': self.request_stats['total'],
+                             'send_failed': self.request_stats['send_failed']
+                             'error': self.request_stats['error']
+                             'timeout': self.request_stats['timeout']
+                             'successful': self.request_stats['success']
+                            }
+                'last_successful_response': last_good,
+                'average_response_time': resp_time
+               }
+        
     def receive(self) -> bool:
         try:
             parts = self.sock.recv_multipart()
+            
+            t_start = time.time()
+            self.request_stats['total'] += 1
             
         except zmq.error.Again:
             return False
@@ -53,13 +116,23 @@ class PredictionServer:
             else:
                 results = self.identify(*parts)
         except Exception as e:
+            self.request_stats['error'] += 1
+            
             if self.logger:
                 self.logger.error(f"Failed to predict request from {parts[0]}, request ID {parts[1]}: {str(e)}")
             return False
             
         try:
             self.sock.send_multipart(results)
+            
+            t_end = time.time()
+            t_resp = t_start - t_end
+            self.request_stats['last_success'] = t_end
+            self.request_stats['response_times'].append(t_resp)
+            
         except zmq.error.Again as e:
+            self.request_stats['send_failed'] += 1
+            
             if self.logger:
                 self.logger.warn(f"Failed to send results on {results[1]} to {results[0]}: {str(e)}")
             return False
