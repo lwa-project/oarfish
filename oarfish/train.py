dc1362fd81e4f948d042924790ca453db94a03a7
jaycedowell <jaycedowell@gmail.com>
2026-09-15 16:34:07 -0600

    Missing )s.

--- oarfish/train.py
+++ oarfish/train.py
@@ -103,8 +103,8 @@ class ModelTrainer:
         """Load model checkpoint"""
         checkpoint = torch.load(checkpoint_path, weights_only=False)
         if 'code_checksum' in checkpoint:
-            cp_data_utils = ','.join([c for i,c in enumerate(checkpoint['code_checksum'].split(',') if i in (0,1,3)])
-            py_data_utils = ','.join([c for i,c in enumerate(CODE_CHECKSUM.split(',') if i in (0,1,3)])
+            cp_data_utils = ','.join([c for i,c in enumerate(checkpoint['code_checksum'].split(',')) if i in (0,1,3)])
+            py_data_utils = ','.join([c for i,c in enumerate(CODE_CHECKSUM.split(',')) if i in (0,1,3)])
             
             if cp_data_utils != py_data_utils:
                 print("Warning: checksum mis-match between software and checkpoint data")
